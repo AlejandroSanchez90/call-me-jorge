d7028c48272166d0d4dcdf9fb72f70d5a1105970
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2023-11-09 12:05:26 -0600

    Update route.ts

--- app/api/sheet/route.ts
+++ app/api/sheet/route.ts
@@ -8,22 +8,26 @@ const GOOGLE_CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
 const GOOGLE_SERVICE_PRIVATE_KEY = process.env.GOOGLE_SERVICE_PRIVATE_KEY;
 
 export async function POST(request: Request) {
-  console.log(SPREADSHEET_ID, SHEET_ID, GOOGLE_CLIENT_EMAIL, GOOGLE_SERVICE_PRIVATE_KEY);
-  const { email, name, phone, postalCode } = await request.json();
-  const influencer = process.env.INFLUENCER!;
+  try {
+    const { email, name, phone, postalCode } = await request.json();
+    const influencer = process.env.INFLUENCER!;
 
-  const serviceAccountAuth = new JWT({
-    email: GOOGLE_CLIENT_EMAIL,
-    key: GOOGLE_SERVICE_PRIVATE_KEY,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID!, serviceAccountAuth);
+    const serviceAccountAuth = new JWT({
+      email: GOOGLE_CLIENT_EMAIL,
+      key: GOOGLE_SERVICE_PRIVATE_KEY,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const doc = new GoogleSpreadsheet(SPREADSHEET_ID!, serviceAccountAuth);
 
-  await doc.loadInfo();
+    await doc.loadInfo();
 
-  const sheet = doc.sheetsById[-SHEET_ID!];
-  await sheet.addRow({ email, name, phone, postalCode, influencer });
-  console.log(doc.title);
+    const sheet = doc.sheetsById[-SHEET_ID!];
+    const info = await sheet.addRow({ email, name, phone, postalCode, influencer });
+    console.log(info);
 
-  return NextResponse.json({});
+    return NextResponse.json({});
+  } catch (error) {
+    console.error(error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
 }
